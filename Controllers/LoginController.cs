68036a7eaed415e5724946c69331d32cd5d50476
ganmecc <995611928@qq.com>
2019-06-04 23:11:20 +0800

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using DataCheckProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataCheckProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //自动映射
+        public IActionResult PostData(UserInfo userinfo)
+        {
+            //验证是否有效
+            return Content(ModelState.IsValid ? "模型数据有效" : "模型数据无效");
+        }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DataCheckProject.Models
+{
+    public class UserInfo
+    {
+        [Required(ErrorMessage ="用户名不能为空")]
+        [StringLength(10,ErrorMessage ="用户名长度不能超过10")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "密码不能为空")]
+        [StringLength(6,ErrorMessage ="密码长度不能超过6")]
+        public string Passward { get; set; }
+    }
+}
